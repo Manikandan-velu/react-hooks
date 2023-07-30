0bc623370a89de81a00867fa44c160f613573a39
Manikandan Velu <54391161+Manikandan-velu@users.noreply.github.com>
2023-07-30 10:41:34 +0300

    Update helper changes

--- src/Utlis/helpers.ts
+++ src/Utlis/helpers.ts
@@ -1,7 +1,8 @@
+//get auth token to validate
 export const isAuth = () => {
     return localStorage.getItem('access_token') ? true : false;
 }
-
+// find attribute for test
 export const findTestAttr = (component: any, attr: string)=> {
   let wrap = component.find(`[data-test='${attr}']`);
   return wrap;
